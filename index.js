eafe0050375439440e3d79afc7b7663b0159194c
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-09-02 18:14:10 +0800

    load test logic

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const EasyGraphQLLoadTester = require('easygraphql-load-tester')
+const fs = require('fs')
+const path = require('path')
+
+const cacheQuerySchema = fs.readFileSync(path.join(__dirname, 'schema', 'schema.graphql'), 'utf8')
+
+const loadTester = new EasyGraphQLLoadTester(cacheQuerySchema)
+
+const queries = [
+  {
+    name: 'events(query: $query)',
+    query: `
+      query ($query:SportMarketQueryInputType!){
+        events(query:$query){
+            eventId,
+            eventCode,
+            awayTeam{
+                teamId
+            }
+        }
+      }
+    `,
+    variables: `query {
+        sportType: "SOCCER",
+        marketPageType: "Live"
+    }
+    `
+  }
+]
+
+const options = {
+  selectedQueries: ['events(query: $query)'],
+  customQueries: queries,
+}
+
+const testCases = loadTester.artillery(options)
+
+module.exports = {
+  testCases
+}
